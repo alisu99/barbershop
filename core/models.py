4032447aa019456abd90bd1311b52a0da26fd286
alisu99 <alissonsts910@gmail.com>
2024-07-16 20:24:33 -0300

    ...

--- core/models.py
+++ core/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Servico(models.Model):
     id = models.AutoField(primary_key=True)
     servico = models.CharField("Serviço", max_length=255, null=False)
@@ -38,20 +37,33 @@ class Usuario(models.Model):
 
 class Agendamento(models.Model):
     id = models.AutoField(primary_key=True)
-    servico_selecionado = models.ForeignKey(Servico, on_delete=models.CASCADE)
-    profissional_selecionado = models.ForeignKey(Profissional, on_delete=models.CASCADE)
-    horario_selecionado = models.ForeignKey(Horario, on_delete=models.CASCADE)
+    servico_selecionado = models.ForeignKey('Servico', on_delete=models.SET_NULL, null=True, blank=True)
+    nome_servico = models.CharField("Nome do Serviço", max_length=255, null=False)
+    profissional_selecionado = models.ForeignKey('Profissional', on_delete=models.SET_NULL, null=True, blank=True)
+    nome_profissional = models.CharField("Nome do Profissional", max_length=255, null=False)
+    horario_selecionado = models.ForeignKey('Horario', on_delete=models.SET_NULL, null=True, blank=True)
+    horario = models.CharField("Horário", max_length=255, null=False)
     data = models.DateField("Data", null=False)
     nome_cliente = models.CharField("Cliente", max_length=255, null=False)
-    telefone_cliente = models.CharField("telefone", max_length=255, null=False)
+    telefone_cliente = models.CharField("Telefone", max_length=255, null=False)
     finalizado = models.BooleanField(default=False)
 
     def __str__(self):
         return self.nome_cliente
 
+    @staticmethod
     def is_horario_disponivel(profissional_id, data, horario_id):
         return not Agendamento.objects.filter(
             profissional_selecionado_id=profissional_id,
             data=data,
             horario_selecionado_id=horario_id
         ).exists()
+    
+    def save(self, *args, **kwargs):
+        if self.servico_selecionado:
+            self.nome_servico = self.servico_selecionado.servico
+        if self.profissional_selecionado:
+            self.nome_profissional = self.profissional_selecionado.nome
+        if self.horario_selecionado:
+            self.horario = self.horario_selecionado.horario
+        super().save(*args, **kwargs)
