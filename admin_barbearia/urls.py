1bb9909096cbb00133d2683963a04db083ebe0f2
alisu99 <alissonsts910@gmail.com>
2024-07-24 00:32:50 -0300

    ...

--- admin_barbearia/urls.py
+++ admin_barbearia/urls.py
@@ -4,7 +4,6 @@ from .views import *
 urlpatterns = [
     path('dashboard/', dashboard, name='dashboard'),
     path('finalizar/<int:id>', finalizar, name='finalizar'),
-    path('adm/', adm, name='adm'),
     path('profissionais/', profissionais, name='profissionais'),
     path('atualizar-disponibilidade/', atualizar_disponibilidade, name='atualizar_disponibilidade'),
     path('adicionar-profissional/', adicionar_profissional, name='adicionar_profissional'),
